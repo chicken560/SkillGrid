344f95da9a5ca4a138a0faf31e0b8d1117ece6a3
chicken560 <braydondavis07@gmail.com>
2026-05-01 10:08:50 -0600

    fix footstep

--- Assets/Scripts/Footsteps.cs
+++ Assets/Scripts/Footsteps.cs
@@ -4,9 +4,9 @@ public class Footsteps : MonoBehaviour
 {
     public AudioSource footsteps;
 
-    void Update[]
+    void Update()
     {
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)){
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)){
             footsteps.enabled = true;
         }
         else
