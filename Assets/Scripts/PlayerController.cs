1e46c1ba0fead6f232455e5d9af6326311b4fad2
chicken560 <braydondavis07@gmail.com>
2026-05-01 10:54:38 -0600

    player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,78 +1,15 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
-    public float jumpForce = 5f;
-    private bool isGrounded = false;
-    Rigidbody rb;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Vector2 move;
+    public void MoveInput(Vector2 newMoveDirection)
     {
-        rb = GetComponent<Rigidbody>();
-        if (rb == null)
-        {
-            Debug.LogWarning("PlayerController: No Rigidbody found. Add a Rigidbody to use jumping.");
-        }
+        move = newMoveDirection;
     }
-
-    // Update is called once per frame
-    void Update()
+    public void OnMove(InputValue value)
     {
-        float horizontalMovement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        float verticalMovement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        transform.Translate(horizontalMovement, 0, verticalMovement);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            if (rb != null)
-            {
-                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            }
-        }
-
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            // Check contact normals to ensure the collision is from below (standing on ground)
-            foreach (ContactPoint contact in collision.contacts)
-            {
-                if (contact.normal.y > 0.5f)
-                {
-                    isGrounded = true;
-                    break;
-                }
-            }
-        }
-    }
-
-    void OnCollisionStay(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            foreach (ContactPoint contact in collision.contacts)
-            {
-                if (contact.normal.y > 0.5f)
-                {
-                    isGrounded = true;
-                    return;
-                }
-            }
-            // If none of the contact normals point mostly up, treat as not grounded
-            isGrounded = false;
-        }
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            // Left the ground object, no longer grounded
-            isGrounded = false;
-        }
+        MoveInput(value.Get<Vector2>());
     }
 }
