00d944d1adf9f92a2210dbe8606b6352c61bd38f
chicken560 <braydondavis07@gmail.com>
2026-05-04 11:29:30 -0600

    start scene UI

---
+++ Assets/UI/SCripts/Play_Button.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public string Scene = "";
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
