796f88ff194b01295ca2c4f3de0b1e7cab07cc62
chicken560 <braydondavis07@gmail.com>
2026-05-19 10:37:24 -0600

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -47,7 +47,7 @@ public class PlayerController : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal"); // Get horizontal input (A/D or Left/Right)    
         float verticalInput = Input.GetAxis("Vertical"); // Get vertical input (W/S or Up/Down)
-        Vector3 move = new Vector3(horizontalInput, 0, verticalInput) * moveSpeed * Time.deltaTime; // Calculate movement vector based on input and speed
+        Vector3 move = new Vector3(horizontalInput, 0, verticalInput) * moveSpeed; // Calculate movement vector based on input and speed
         if (Input.GetButtonDown("Jump") && isGrounded) // Check if the jump button is pressed and the player is grounded
         {
             Jump(); // Apply jump force to the player's Rigidbody
@@ -67,7 +67,8 @@ public class PlayerController : MonoBehaviour
                 StopSprint();
             }
         }
-        transform.Translate(move); // Move the player based on the calculated movement and jump vectors
+        // Apply movement using Rigidbody velocity
+
     }
 
     void Jump()
